c787e06686a8073aa9ce25effd4e529d8edb169e
Eachan Johnson <eachan.johnson@crick.ac.uk>
2024-02-22 00:31:56 +0000

    Fix imports

--- carabiner/__init__.py
+++ carabiner/__init__.py
@@ -1,3 +1,4 @@
-from .utils import *
+from .utils import (colorblind_palette, print_err, 
+                    pprint_dict, upper_and_lower)
 from . import (cast, cliutils, collections, 
                decorators, itertools, random)
