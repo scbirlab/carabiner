1719db84660177c11656a5e975f83b1f74e864b9
Eachan Johnson <eachan.johnson@crick.ac.uk>
2024-10-05 14:19:40 +0100

    Set default color cycler #3, turn ticks back on in  #5, add scattergrid function #4

--- carabiner/mpl/utils.py
+++ carabiner/mpl/utils.py
@@ -1,25 +1,38 @@
 """Utilities for matplotlib."""
 
-from typing import Tuple
+from typing import Any, Iterable, Mapping, Tuple, Optional
+from functools import wraps
 
+from pandas import DataFrame
 try:
     import matplotlib.pyplot as plt
 except ImportError:
-    raise ImportError("\Matplotlib not installed. Try installing with pip:"
-                      "\n$ pip install matplotlib\n"
+    raise ImportError("Matplotlib not installed. Try installing with pip:"
+                      "\n\t$ pip install matplotlib\n"
                       "\nor reinstall carabiner with matplotlib:\n"
-                      "\n$ pip install carabiner[mpl]\n")
+                      "\n\t$ pip install carabiner[mpl]\n")
 else:
-    from matplotlib import figure, axes
+    from matplotlib import axes, cycler, figure, rcParams
+
+from ..cast import cast
+from ..utils import colorblind_palette as utils_colorblind_palette
+rcParams['axes.prop_cycle'] = cycler(color=utils_colorblind_palette())
 
 TFigAx = Tuple[figure.Figure, axes.Axes]
 
-def grid(nrow: int = 1, 
-         ncol: int = 1, 
-         panel_size: float = 3., 
-         aspect_ratio: float = 1., 
-         layout: str = 'constrained',
-         *args, **kwargs) -> TFigAx:
+colorblind_palette = utils_colorblind_palette
+
+def grid(
+     nrow: int = 1, 
+     ncol: int = 1, 
+     panel_size: float = 3., 
+     aspect_ratio: float = 1., 
+     layout: str = 'constrained',
+     sharex: Union[str, bool] = False,
+     sharey: Union[str, bool] = False,
+     hide_shared_ticks: bool = False,
+     *args, **kwargs
+) -> TFigAx:
     
      """Create a figure and a set of subplots with sensible defaults.
 
@@ -34,9 +47,12 @@ def grid(nrow: int = 1,
      panel_size : float, optional
           Size of panels. Default: 3.
      aspect_ratio : float
-          Ratio of width over height. Default: 1. (square).
+          Ratio of width over height. Default: 1 (square).
      layout : str
           Matplotlib `fig` layout. Default: "constrained".
+     hide_shared_ticks : bool, optional
+          Whether to hide the ticks when axes have shared scales
+          from setting `sharex` or `sharey`. Default: `False`.
 
      Returns
      -------
@@ -44,9 +60,147 @@ def grid(nrow: int = 1,
           Pair of `figure.Figure` and `axes.Axes` objects.
     
      """
+     figsize=(
+          ncol * panel_size * aspect_ratio,
+          nrow * panel_size,
+     )
+     fig, axes = plt.subplots(
+          nrow, 
+          ncol, 
+          figsize=figsize,
+          layout=layout,
+          sharex=sharex,
+          sharey=sharey,
+          *args, **kwargs
+     )
+     if sharex: 
+          for ax in axes:
+               ax.xaxis.set_tick_params(labelbottom=True)
+     if sharey:
+          for ax in axes:
+               ax.yaxis.set_tick_params(labelleft=True)
+     return fig, axes
 
-     return plt.subplots(nrow, ncol, 
-                         figsize=(ncol * panel_size * aspect_ratio,
-                                  nrow * panel_size),
-                         layout=layout,
-                         *args, **kwargs)
+def scatter_grid(
+     df: DataFrame, 
+     grid_columns: Union[str, Iterable[str]], 
+     grid_rows: Optional[Union[str, Iterable[str]]] = None, 
+     grouping: Optional[Union[str, Iterable[str]]] = None,
+     log: Optional[Union[str, Iterable[str]]] = None,
+     n_bins: int = 40,
+     scatter_opts: Optional[Mapping[str, Any]] = None,
+     legend_opts: Optional[Mapping[str, Any]] = None,
+     *args, **kwargs
+) -> TFigAx:
+
+     """Create a scatter plot to compare sets of variables in a Pandas DataFrame.
+
+     Similar to `pandas.plotting.scatter_matrix`, but with larger panels and control
+     over which variables are log-scaled. 
+     
+     Additional arguments are passed to `grid()`.
+
+     Parameters
+     ----------
+     df : pandas.DataFrame
+          Data to plot.
+     grid_columns : str | Iterable[str]
+          Data columns to plot along the rows of the scatter grid. 
+          Becomes the x-axes.
+     grid_rows : str | Iterable[str], optional
+          Data columns to plot down the columns of the scatter grid. 
+          Becomes the y-axes. If not provided, uses `grid_columns` for
+          all pair-wise comparison.
+     grouping : str | Iterable[str], optional
+          If provided, use these columns of `df` to make groups and plot each 
+          data group as a differnt color.
+     log : str | Iterable[str], optional
+          If provided, plot these columns of `df` on a log scale.
+     n_bins : int, optional
+          Number of bin for histograms plotted on identity diagonal of the 
+          scatter grid. Default: 40.
+     scatter_opts : dict, optional
+          Extra keyword arguments to pass to the Matplotlib scatter plots.
+     legend_opts : dict, optional
+          Extra keyword arguments to pass to the Matplotlib legend.
+
+     Returns
+     -------
+     tuple
+          Pair of `figure.Figure` and `axes.Axes` objects.
+
+     Raises
+     ------
+     KeyError
+          If no named columns are in `df`.
+    
+     """
+
+     grid_columns = [name for name in cast(grid_columns, to=list) if name in df]
+     grid_rows = grid_rows or grid_columns
+     grid_rows = [name for name in cast(grid_rows, to=list) if name in df]
+     all_names = sorted(set(grid_columns + grid_rows))
+
+     log = log or []
+     log = [name for name in cast(log, to=list) if name in all_names]
+
+     _scatter_opts = {"s": 3.}
+     _scatter_opts.update(scatter_opts or {})
+     _legend_opts = {"loc": "center left", "bbox_to_anchor": (1., .5)}
+     _legend_opts.update(legend_opts or {})
+
+     if grouping is None:
+          grouping = "__group__"
+          df = df.assign(__group__=grouping).groupby(grouping)
+          dummy_group = True
+     else:
+          grouping = sorted(set([
+               g for g in cast(grouping, to=list) if g in df
+          ]))
+          if len(grouping) == 0:
+               raise KeyError(f"No columns in grouping ({', '.join(grouping)}) were in the DataFrame ({', '.join(df)})!")
+          df = df.groupby(grouping)
+          dummy_group = False
+
+     fig, axes = grid(
+          nrow=len(grid_rows), 
+          ncol=len(grid_columns),
+          *args, **kwargs
+     )
+     for axrow, grid_row_name in zip(tqdm(axes), grid_rows):
+          yscale = "log" if grid_row_name in log else "linear"
+          for ax, grid_col_name in zip(axrow, grid_columns):
+               xscale = "log" if grid_col_name in log else "linear"
+               for group_name, group_df in df:
+                    extras = {"label": group_name} if not dummy_group else {}
+                    if grid_row_name == grid_col_name:
+                         if xscale == "log":
+                              bins = np.geomspace(
+                                   values.min(), 
+                                   values.max(), 
+                                   num=n_bins,
+                              )
+                         else:
+                              bins = n_bins
+                         ax.hist(
+                              grid_col_name, 
+                              data=group_df, 
+                              bins=bins,
+                              **extras
+                         )
+                         ylabel = "Frequency"
+                    else:
+                         ax.scatter(
+                              grid_col_name,
+                              grid_row_name, 
+                              data=group_df, 
+                              **_scatter_opts,
+                              **extras
+                         )
+               ylabel = grid_row_name
+               ax.set(
+                    xlabel=grid_col_name, 
+                    ylabel=ylabel,
+               )
+          ax.legend(**_legend_opts)
+     return fig, axes
